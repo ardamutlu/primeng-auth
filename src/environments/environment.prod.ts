31acb571b75cdac9dfc322a9c5ee95c203ed03e1
Arda Mutlu <ardamutlu@Ardas-MacBook-Pro.local>
2022-12-13 21:38:41 +0300

    create app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  isMockEnabled: false,
+  appVersion: 'v1.0.0',
+  apiUrl: 'api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  isMockEnabled: true,
+  appVersion: 'v1.0.0',
+  apiUrl: 'api',
+};
